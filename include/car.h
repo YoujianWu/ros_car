442572d977a7789c7452a800d89daa0c644cefc1
youjian <1124895509@qq.com>
2025-02-25 10:04:36 +0800

    Add template

---
+++ include/car.h
@@ -0,0 +1,6 @@
+//
+// Created by kook on 2/25/25.
+//
+
+#pragma once
+
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by kook on 2/25/25.
+//
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+
+#include "car.h"
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "car");
+    return 0;
+}
